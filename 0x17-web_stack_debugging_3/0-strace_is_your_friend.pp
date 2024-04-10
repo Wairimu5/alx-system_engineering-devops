4f6dcb2b87c1172be1701fcf0d69d1cab730f83a
Wairimu5 <innonjue@gmail.com>
2024-04-10 14:47:59 -0700

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,15 @@
-# Puppet manifest to fix typo in WordPress file wp-settings.php using strace
+# Puppet manifest to fix typo in WordPress file wp-settings.php
 
-exec { 'fix-wordpress':
-  command     => 'strace -o /tmp/strace_output sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin/:/bin/',
-  refreshonly => true,
+# Fix the typo in wp-settings.php
+exec { 'fix-wordpress-typo':
+  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path        => ['/usr/bin', '/bin'],  # Specify the path to sed
+  unless      => 'grep -q "php" /var/www/html/wp-settings.php',  # Check if the file has already been fixed
+  onlyif      => 'test -f /var/www/html/wp-settings.php',  # Ensure the file exists
 }
 
+# Ensure the wp-settings.php file exists
 file { '/var/www/html/wp-settings.php':
   ensure  => file,
-  require => Exec['fix-wordpress'],
+  require => Exec['fix-wordpress-typo'],  # Make sure the fix occurs before ensuring file existence
 }
